3131d23e6e1e5db9c9fab28f5efa2c55570d3ba0
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2019-04-17 23:41:25 +0200

    add show article

--- src/Controller/LinkController.php
+++ src/Controller/LinkController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundel\FrameworkExtraBundle\Configuration\Method;
@@ -14,10 +16,7 @@ class LinkController extends Controller
      */
     public function index()
     {
-        $articles = [
-            'Article 1',
-            'Article 2'
-        ];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         $person = [
             'name'=>'patryk',
@@ -28,8 +27,36 @@ class LinkController extends Controller
             'person'=>$person,
             'articles'=>$articles
         ];
-  
+
      //   return new Response('<html>HHHH</html>');
-        return $this->render("articles/index.html.twig", array('articles'=> $articles, 'data'=>$data));
+        return $this->render("articles/index.html.twig", array('articles'=> $articles));
+
     }
+
+    /**
+     * @Route("/article/{id}" , name="article_show");
+     */
+
+     public function show($id){
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render("articles/show.html.twig", array('article'=>$article));
+     }
+
+    /**
+     * @Route("/article/save")
+     */
+
+    // public function save(){
+    //     $entityManager = $this->getDoctrine()->getManager();
+
+    //     $article = new Article();
+    //     $article->setTitle('Article secondary');
+    //     $article->setBody('This is a body of two');
+
+    //     $entityManager->persist($article);
+    //     $entityManager->flush();
+
+    //     return new Response('Zapisano jako art o numerze '. $article->getId());
+    // }
 }
