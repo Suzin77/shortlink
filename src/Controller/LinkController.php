aef72e25f08fcec7fccceee4f82895e412e73670
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2019-04-18 12:18:57 +0200

    add new article
    add new article

--- src/Controller/LinkController.php
+++ src/Controller/LinkController.php
@@ -5,13 +5,19 @@ use App\Entity\Article;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 use Sensio\Bundel\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class LinkController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name ="article_list")
      * 
      */
     public function index()
@@ -34,14 +40,54 @@ class LinkController extends Controller
     }
 
     /**
-     * @Route("/article/{id}" , name="article_show");
+     * @Route("/article/new", name="article_new");
+     * Method({"GET", "POST"})
      */
 
-     public function show($id){
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class, array('attr'=>array('class'=> 'form-control')))
+                     ->add('body', TextareaType::class, array(
+                           'required'=> false,
+                           'attr'=>[
+                               'class'=>'form-control'
+                           ]
+                       ))
+                     ->add('Save', SubmitType::class, array(
+                         'label'=> 'Create',
+                         'attr' => [
+                             'class' => 'btn btn-primary mt-3'
+                         ]
+                     ))
+                     ->getForm();
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $article = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', ['form'=>$form->createView()]);
+    }
+
+    /**
+     * @Route("/article/show/{id}" , name="article_show");
+     */
+
+    public function show($id)
+    {
         $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
         return $this->render("articles/show.html.twig", array('article'=>$article));
-     }
+    }
 
     /**
      * @Route("/article/save")
